651d7823671e8186070470714c2b82bcccdb3386
Ukana.Bastreet <s.ppp@bk.ru>
2025-01-18 15:32:58 +0300

    express server init

---
+++ backend/src/main.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+const PORT = process.env.APP_PORT;
+
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
